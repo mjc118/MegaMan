e6d1016de70c1692f2b4a6107c28f40015e6fb91
Michael Crabtree <crabtreefalls2009@gmail.com>
2015-03-24 22:42:56 -0400

    Added JellyFish Enemy

--- Megaman/Assets/Scripts/EnemyScripts/JellyFish/JellyFishAI.cs
+++ Megaman/Assets/Scripts/EnemyScripts/JellyFish/JellyFishAI.cs
@@ -1,47 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SeaHorseAI : MonoBehaviour {
+public class JellyFishAI : MonoBehaviour {
 
-    Animator SeaHorseAnim;
+    Animator JellyFishAnim;
+
+    public float AttackDurationTime;//keeps track of how long it originally was
+    float AttackDurationTimeLeft;//decrements in our coroutine
+    public float AttackCooldownDuration;
+    float AttackCooldownLeft = 0;
 
     public float Health;
     public float MoveSpeed;
     bool Dieing = false;
     bool FacingRight;
 
-    bool Spawning = true;
     bool Attacking = false;
 
     //need to track where player is in regards to spawn point
     Transform PlayerPos;
-    float InitialXPosition;
 
-    public float InitialYPosition;
     public GameObject DeathExplosion;
 
     public AudioSource BusterShotSoundSource;
     public AudioClip[] BusterShotSound;
-	
+
     // Use this for initialization
-	void Start () {
-        SeaHorseAnim = GetComponent<Animator>();
+    void Start()
+    {
+        JellyFishAnim = GetComponent<Animator>();
         PlayerPos = GameObject.Find("Character").transform;
-        InitialYPosition = transform.position.y;
-        InitialXPosition = transform.position.x;
 
         transform.parent.gameObject.GetComponent<EnemySpawnPoint>().FacingRight = false;
-        StartCoroutine("InitialSpawn");
-	}
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         FacingRight = transform.parent.gameObject.GetComponent<EnemySpawnPoint>().FacingRight;
-
-        if (Mathf.Abs(transform.position.x - InitialXPosition) > 15f)
-        {
-            InvokeDeath();
-        }
 
         if (Health <= 0 && !Dieing)
         {
@@ -50,53 +45,45 @@ public class SeaHorseAI : MonoBehaviour {
             InvokeDeath();
         }
 
-        if (!Attacking)
+        if (!Dieing)
         {
-            if (PlayerPos.position.x - transform.position.x < 0 && FacingRight)
-            {
-                Flip();
-            }
-            else if (PlayerPos.position.x - transform.position.x > 0 && !FacingRight)
-            {
-                Flip();
-            }
-        }
-        else
-        {
-            if (!FacingRight) 
-            {
-                transform.Translate(Vector3.left * MoveSpeed);
-            }
-            else
-            {
-                transform.Translate(Vector3.right * MoveSpeed);
-            }
-        }
+            if (AttackCooldownLeft > 0) { AttackCooldownLeft -= Time.deltaTime; }
 
+            float DistanceFromPlayer = Mathf.Sqrt(Mathf.Pow(PlayerPos.position.x - transform.position.x, 2) + Mathf.Pow(PlayerPos.position.y - transform.position.y, 2));
+
+            if (!Attacking && AttackCooldownLeft <= 0 && DistanceFromPlayer < 3f)
+            {
+                if (Random.Range(1, 10) < 4)
+                {
+                    Attacking = true;
+                    JellyFishAnim.SetBool("Attack", Attacking);
+                    AttackDurationTimeLeft = AttackDurationTime;
+                    StartCoroutine("Attack");
+                }
+                else
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, PlayerPos.position, Time.deltaTime * MoveSpeed);
+                }
+            }
+            else if (!Attacking)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, PlayerPos.position, Time.deltaTime * MoveSpeed);
+            }
+
+            if (!Attacking)
+            {
+                if (PlayerPos.position.x - transform.position.x < 0 && FacingRight)
+                {
+                    Flip();
+                }
+                else if (PlayerPos.position.x - transform.position.x > 0 && !FacingRight)
+                {
+                    Flip();
+                }
+            }
+        }
         //set the public bool in the parent that is referenced by MegamanMovement to it's correct state
         transform.parent.gameObject.GetComponent<EnemySpawnPoint>().FacingRight = FacingRight;
-
-	}
-
-    IEnumerator InitialSpawn()
-    {
-        Spawning = true;
-        SeaHorseAnim.SetBool("Spawning", Spawning);
-
-        while (Mathf.Abs(transform.position.y - InitialYPosition) < 5f)
-        {
-            transform.Translate(Vector3.up * MoveSpeed);
-            yield return 0;
-        }
-
-        yield return new WaitForSeconds(0.5f);
-        
-        Spawning = false;
-        SeaHorseAnim.SetBool("Spawning", Spawning);
-
-        yield return new WaitForSeconds(1f);
-
-        Attacking = true;
     }
 
     //flip the sprites
@@ -106,6 +93,19 @@ public class SeaHorseAI : MonoBehaviour {
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
+    }
+
+    IEnumerator Attack()
+    {
+        while (AttackDurationTimeLeft > 0)
+        {
+            AttackDurationTimeLeft -= Time.deltaTime;
+            yield return 0;
+        }
+        Attacking = false;
+        JellyFishAnim.SetBool("Attack", Attacking);
+
+        AttackCooldownLeft = AttackCooldownDuration;
     }
 
     void OnTriggerEnter2D(Collider2D trigger)
@@ -132,5 +132,4 @@ public class SeaHorseAI : MonoBehaviour {
         transform.parent.gameObject.GetComponent<EnemySpawnPoint>().IsThisEnemyAlive = false;
         Destroy(gameObject);
     }
-
 }
