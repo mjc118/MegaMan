1aad8561087dcedcf3d109dbafd1b7bdddfd31fa
Michael Crabtree <crabtreefalls2009@gmail.com>
2015-03-28 22:47:55 -0400

    Miner Enemy Done

---
+++ Megaman/Assets/Scripts/EnemyScripts/Miner/AxeAI.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AxeAI : MonoBehaviour {
+
+    Animator AxeAnim;
+    public Transform TerrainCheck;
+    public bool HitTerrain;
+    public LayerMask WhatIsTerrain;
+
+    bool Spawning = true;
+
+    Vector3 ArcBegin, ArcEnd; //VectorPositions that mark the start and end
+    public float ArcHeight; //desired parabola height
+    float ArcTimeElapsed = 0;//How many seconds into the current jump we are
+    public float ArcDuration;//#seconds a jump takes to complete
+
+	// Use this for initialization
+	void Start () {
+        if (GetComponentInParent<MinerAI>().FacingRight)
+        {
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+
+        ArcBegin = transform.position;
+        ArcEnd = GameObject.Find("Character").transform.position;
+	    AxeAnim = GetComponent<Animator>();
+        Invoke("SetSpawnFalse", 0.25f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    HitTerrain = Physics2D.OverlapCircle(TerrainCheck.position, 0.1f, WhatIsTerrain);
+
+        if(HitTerrain){
+            this.GetComponentInParent<MinerAI>().AxeIsAlive = false;
+            Destroy(gameObject);
+        }
+
+        if (!Spawning)
+        {
+            float objectT = ArcTimeElapsed % ArcDuration; //completes the parabola trip in JumpDuration Time
+            transform.position = ArcParabola(ArcBegin, ArcEnd, ArcHeight, (objectT / ArcDuration) * 4);
+
+            ArcTimeElapsed += Time.deltaTime;
+        }
+	}
+
+    void SetSpawnFalse(){
+        Spawning = false;
+        AxeAnim.SetBool("Spawning", Spawning);
+    }
+
+    void OnTriggerEnter2D(Collider2D trigger)
+    {
+        if (trigger.tag == "Player")
+        {
+            if (!trigger.gameObject.GetComponent<MegamanMovement>().CurrentlyInvulnerable)
+            {
+                trigger.GetComponent<MegamanMovement>().CurrentlyInvulnerable = true;
+                trigger.GetComponent<MegamanMovement>().Health -= 2;
+                trigger.GetComponent<MegamanMovement>().StartCoroutine("InvulnerabilityFrames", this.GetComponentInParent<MinerAI>().FacingRight);
+            }
+            this.GetComponentInParent<MinerAI>().AxeIsAlive = false;
+            Destroy(gameObject);
+        }
+    }
+
+    Vector3 ArcParabola(Vector3 start, Vector3 end, float height, float t)
+    {
+        float parabolicT = t * 2 - 1;
+
+        if (true)//Mathf.Abs(start.y - end.y) < 0.1f)
+        {
+            //start and end are roughly level, pretend they are - simpler solution with less steps
+            Vector3 travelDirection = end - start;
+            Vector3 result = start + t * travelDirection;
+            result.y += (-parabolicT * parabolicT + 1) * height;
+            return result;
+        }
+        else
+        {
+            //start and end are not level, gets more complicated
+            Vector3 travelDirection = end - start;
+            Vector3 levelDirecteion = end - new Vector3(start.x, end.y, start.z);
+            Vector3 right = Vector3.Cross(travelDirection, levelDirecteion);
+            Vector3 up = Vector3.Cross(right, travelDirection);
+            if (end.y > start.y) up = -up;
+            Vector3 result = start + t * travelDirection;
+            result += ((-parabolicT * parabolicT + 1) * height) * up.normalized;
+            return result;
+        }
+    }
+}
---
+++ Megaman/Assets/Scripts/EnemyScripts/Miner/MinerAI.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinerAI : MonoBehaviour {
+
+    Animator MinerAnim;
+
+    public float Health;
+    bool Dieing = false;
+    public bool FacingRight;
+
+    //need to track where player is in regards to spawn point
+    Transform PlayerPos;
+
+    public GameObject DeathExplosion;
+    public GameObject AxePrefab;
+    public bool AxeIsAlive = false;
+    float AttackCooldown = 3f;
+
+    public AudioSource BusterShotSoundSource;
+    public AudioClip[] BusterShotSound;
+
+	// Use this for initialization
+	void Start () {
+        MinerAnim = GetComponent<Animator>();
+        PlayerPos = GameObject.Find("Character").transform;
+
+        transform.parent.gameObject.GetComponent<EnemySpawnPoint>().FacingRight = false;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        FacingRight = transform.parent.gameObject.GetComponent<EnemySpawnPoint>().FacingRight;
+
+        if (Health <= 0 && !Dieing)
+        {
+            Dieing = true;
+            Instantiate(DeathExplosion, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+            InvokeDeath();
+        }
+
+        if (!Dieing)
+        {
+            if (PlayerPos.position.x - transform.position.x < 0 && FacingRight)
+            {
+                Flip();
+            }
+            else if (PlayerPos.position.x - transform.position.x > 0 && !FacingRight)
+            {
+                Flip();
+            }
+
+
+            if (!AxeIsAlive)
+            {
+                if (Mathf.Abs(transform.position.x - PlayerPos.position.x) < 15f)
+                {
+                    if (AttackCooldown > 0)
+                    {
+                        AttackCooldown -= Time.deltaTime;
+                    }
+                    else
+                    {
+                        AttackCooldown = 3f;
+                        AxeIsAlive = true;
+                        MinerAnim.SetBool("Attack", true);
+                        Invoke("ThrowAxe", 0.3f);
+                    }
+                }
+            }
+        }
+        //set the public bool in the parent that is referenced by MegamanMovement to it's correct state
+        transform.parent.gameObject.GetComponent<EnemySpawnPoint>().FacingRight = FacingRight;
+	}
+
+    void ThrowAxe()
+    {
+        Vector3 AxePos;
+        
+        AxePos = new Vector3(transform.position.x + (transform.localScale.x/-10.4727745098f), transform.position.y + 0.4041f, transform.position.z);
+
+        GameObject Axe = Instantiate(AxePrefab, AxePos, Quaternion.identity) as GameObject;
+        Axe.transform.parent = this.transform;
+
+        Invoke("SetAttackFalse", 1f);
+    }
+
+    void SetAttackFalse()
+    {
+        MinerAnim.SetBool("Attack", false);
+    }
+
+    //flip the sprites
+    void Flip()
+    {
+        FacingRight = !FacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    void InvokeDeath()
+    {
+        transform.parent.gameObject.GetComponent<EnemySpawnPoint>().IsThisEnemyAlive = false;
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D trigger)
+    {
+        if (trigger.gameObject.tag == "BusterShot")
+        {
+            BusterShotSoundSource.PlayOneShot(BusterShotSound[0]);
+            Health -= 1f;
+        }
+        else if (trigger.gameObject.tag == "BusterShotMedium")
+        {
+            BusterShotSoundSource.PlayOneShot(BusterShotSound[1]);
+            Health -= 2f;
+        }
+        else if (trigger.gameObject.tag == "BusterShotLarge")
+        {
+            BusterShotSoundSource.PlayOneShot(BusterShotSound[1]);
+            Health -= trigger.gameObject.GetComponent<LargeBusterShot>().ShotDamage;
+        }
+    }
+}
